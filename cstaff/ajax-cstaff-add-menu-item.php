89b021b1b444f687b419aa195fc8212a9fcdf930
wtfoong <wtfoong81@gmail.com>
2022-11-07 13:43:28 +0800

    successfully added image to S3 bucket

--- cstaff/ajax-cstaff-add-menu-item.php
+++ cstaff/ajax-cstaff-add-menu-item.php
@@ -1,7 +1,19 @@
 <?php session_start();
 include("../conn_db.php");
+require '../aws/aws-autoloader.php';
 // File upload folder 
 $uploadDir = '/img/menu/';
+
+$s3Client = new Aws\S3\S3Client([
+	'region'  => 'us-east-1',
+	'version' => 'latest',
+    'scheme' => 'http',
+	'credentials' => [
+	    'key'    => "ASIAXOBIIX4WQ3SNDUXC",
+	    'secret' => "LnG3Y87QKjFOkK2bVsUBPxd6Z9xxCWa8zhPzRPw2",
+        'token'=>"FwoGZXIvYXdzEDsaDPDe5RWDN3jgOpcrCiLJAaFTXYr1x/cRzVxfopXchpZTL0L7uZmf4A2HWPFQZ2JLp0PNTVpTHqBcsFsWUS4VrqlkiNo4/Mb0KbJLlfgQAZxhjrea9a47ZpgoIvWhtQjk1vXKzVJMn/hfmKqB21CTvjGpbvqbG1k9SkO/LypYyW7g5PF5E2GSqFI6rZksWzVQZFzCTpjlu1V/caik1IXMQ5yDoJEWaNRfKYKcCTPDvJJhy61PqLM1zk48TPo9adApc1lBg7u44wJTw4y3RAff1mh5Wz2rS3qUryiPt6GbBjItWo+EJgsnkDyluD//EL86bzMV1EI/br3xbH86HDfA4RFnTqUfr7Mt7GuRcXRZ"
+	]
+]);
 
 // Allowed file types 
 $allowTypes = array('png');
@@ -27,18 +39,42 @@ if (mysqli_num_rows($result)) {
     $targetFilePath = $uploadDir . $fileName;
     $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
     if (in_array($fileType, $allowTypes)) {
-        // Upload file to the server 
+
         $mitem_id = $mysqli->insert_id;
-        $target_dir = '/img/menu/';
-        $temp = explode(".", $_FILES["mitem-pic"]["name"]);
-        $target_newfilename = "mitem_id_" . $mitem_id . "." . strtolower(end($temp));
-        $target_file = $target_dir . $target_newfilename;
-        if (move_uploaded_file($_FILES["mitem-pic"]["tmp_name"], SITE_ROOT . $target_file)) {
-            $insert_query = "UPDATE mitem SET mitem_pic = '{$target_newfilename}' WHERE mitem_id = {$mitem_id} AND store_id = {$store_id} ";
+        $bucket = 'ddac-pastry-tp053060';
+        $file_Path = __DIR__ . '/img/'. $fileName;
+        $temp_file_location = $_FILES['mitem-pic']['tmp_name'];
+        $key = basename($file_Path);
+        try {
+            $result = $s3Client->putObject([
+            'Bucket' => $bucket,
+            'Key'    => $key,
+            'Body'   => $temp_file_location,
+            'SourceFile' => $temp_file_location,
+            'ACL'    => 'public-read', // make file 'public'
+            'ContentType' => 'image/png',
+            ]);
+            
+            $image_path = $result->get('ObjectURL');
+            echo "Image uploaded successfully. Image path is: ".$image_path;
+            $insert_query = "UPDATE mitem SET mitem_pic = '{$image_path}' WHERE mitem_id = {$mitem_id} AND store_id = {$store_id} ";
             $insert_result = $mysqli->query($insert_query);
-        } else {
-            $insert_result = false;
+        } catch (Aws\S3\Exception\S3Exception $e) {
+            echo "There was an error uploading the file.\n";
+            echo $e->getMessage();
         }
+        // Upload file to the server 
+        // $mitem_id = $mysqli->insert_id;
+        // $target_dir = '/img/menu/';
+        // $temp = explode(".", $_FILES["mitem-pic"]["name"]);
+        // $target_newfilename = "mitem_id_" . $mitem_id . "." . strtolower(end($temp));
+        // $target_file = $target_dir . $target_newfilename;
+        // if (move_uploaded_file($_FILES["mitem-pic"]["tmp_name"], SITE_ROOT . $target_file)) {
+        //     $insert_query = "UPDATE mitem SET mitem_pic = '{$target_newfilename}' WHERE mitem_id = {$mitem_id} AND store_id = {$store_id} ";
+        //     $insert_result = $mysqli->query($insert_query);
+        // } else {
+        //     $insert_result = false;
+        // }
 
         if ($insert_result) {
             $response['server_status'] = 1;
--- cstaff/cstaff-mng-menu.php
+++ cstaff/cstaff-mng-menu.php
@@ -64,7 +64,8 @@
                         data: 'mitem_pic',
                         render: function(data) {
                             if (data != "") {
-                                return '<img src="../img/menu/' + data + "?" + new Date().getTime() + '"class="img-fluid rounded" width="125px" height="120px"/>'
+                                //return '<img src="../img/menu/' + data + "?" + new Date().getTime() + '"class="img-fluid rounded" width="125px" height="120px"/>'
+                                return '<img src="' + data +"?" + new Date().getTime() +'"class="img-fluid rounded" width="125px" height="120px"/>'
                             } else {
                                 return ''
                             }
